90d7b72a0f82eb02fee1d5c7b4f3fddde15819b4
AbdullahAlMamun02 <48121282+AbdullahAlMamun02@users.noreply.github.com>
2023-06-23 23:35:46 +0600

    week 04 day 01

---
+++ Week 03/Day 05/5_a_b.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        string s;
+        cin>>s;
+        int a = (int)s[0]-'0';
+        int b = (int)s[2]-'0';
+        cout<<a+b<<endl;
+
+    }
+
+
+    return 0;
+}
+
+
