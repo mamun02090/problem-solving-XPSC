9e6df719956c4807c663e4d9d87c2bd052e79dc6
mamun02 <mamun170902@gmail.com>
2023-07-07 23:50:33 +0600

    week 05 day01

---
+++ Week 05/Day 01/_even_make.cpp
@@ -0,0 +1,61 @@
+
+
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        long long n;
+        cin>>n;
+        if(n%2==0) {
+            cout<<0<<endl;
+            continue;
+        }
+        string s = to_string(n);
+        int ans = -1;
+        for(int i=0; i<s.size(); i++){
+            int tmp1 =0;
+            int tmp2 = 0;
+            if((s[i]+'0')%2==0){
+
+                tmp1 = i+1;
+            }
+            if((s[s.size()-i-1]+'0')%2==0){
+                tmp2 = 2;
+            }
+            if(tmp1 !=0 && tmp2!=0 )
+                {
+                    ans = min(tmp1, tmp2);
+                    break;
+                }
+            else if(tmp1!=0)
+                {
+                    ans = tmp1;
+                    break;
+                }
+            else if(tmp2 !=0) {
+                ans = tmp2;
+                break;
+            }
+
+
+        }
+        cout<<ans<<endl;
+    }
+
+
+    return 0;
+}
+
+
