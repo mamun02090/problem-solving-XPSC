365a2e0b378695e336b293951d44a79e4964c03b
mamun02 <mamun170902@gmail.com>
2023-09-06 23:41:53 +0600

    Day 06

---
+++ Week 13/Day 06/1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <map>
+
+using namespace std;
+
+long long M = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
